69ad8e0c697b75d59cec8ff304f2795de22bc252
Simeon052 <simeon_the_city_who_fought@outlook.com>
2017-10-15 09:35:19 +0900

    Time lapse works well

--- CreateTimeLapse/Program.cs
+++ CreateTimeLapse/Program.cs
@@ -3,13 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Util;
 
 namespace CreateTimeLapse
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            if (args.Length >= 3)
+            {
+                string url = args[0];
+                string outputPath = args[1];
+                int recordingMinutes = int.Parse(args[2]);
+                int interval = 1;
+                int fps = 30;
+                string username = null;
+                string password = string.Empty;
+                if(args.Length >= 7)
+                {
+                    interval = int.Parse(args[3]);
+                    fps = int.Parse(args[4]);
+                    username = args[5];
+                    password = args[6];
+                }
+
+                var result = await TimeLapse.GetAsync(url, outputPath, recordingMinutes * 60, interval, fps, username, password);
+                System.Console.WriteLine($"result : {result ?? "convert failed"}");
+            }
+            else
+            {
+                Console.WriteLine($"usage : <url> <output_avi_path> <Recording hour> [interval = 1] [fps = 30] [Username] [Password]");
+            }
         }
     }
 }
